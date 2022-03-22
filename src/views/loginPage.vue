81f4e56cd356d4d9c454d9530cfea24638684ad8
ZTjack <jack.jiang.scivan@gmail.com>
2022-03-22 13:38:22 +0800

    add pinia store

--- src/views/loginPage.vue
+++ src/views/loginPage.vue
@@ -1,3 +1,22 @@
+<script setup lang="ts">
+import { MainStore } from '@/store/main'
+
+const mainStore = MainStore()
+
+const updateName = () => {
+  // $patch 修改 store 中的数据
+  mainStore.$patch({
+    name: '名称被修改了,nameLength也随之改变了'
+  })
+}
+</script>
+
 <template>
-  <h1>login Page1</h1>
+  <div>
+    用户名:{{ mainStore.name }}
+    <br />
+    长度:{{ mainStore.nameLength }}
+  </div>
+  <hr />
+  <button @click="updateName">修改store中的name</button>
 </template>
